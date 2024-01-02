34f9dd3ccb898b2865c91ef420c3a02c0de0c679
blackspider <blackspiderking666@gmail.com>
2024-01-02 16:12:43 +0800

    up

--- application/http/middleware/Check.php
+++ application/http/middleware/Check.php
@@ -10,9 +10,15 @@ class Check
     {
         $uid = Session::get('uid_sign');
 
-        if (empty($uid) && $_SERVER['REQUEST_URI'] != '/login' && $_SERVER['REQUEST_URI'] != '/tologin') {
-            return redirect('/login');
-        }
+        $requestUri = ltrim($_SERVER['REQUEST_URI'], '/'); 
+        
+        $arr = array('douyin_table_all','baidu_table_check','baidu_table_info','kuaishou_table_all','gdt_click_table_all','gdt_exposure_table_all','gdt_follow_table_all','gdt_effect_table_all','gdt_fans_table_all');
+        
+        if (!in_array($requestUri, $arr)) {
+             if (empty($uid) && $_SERVER['REQUEST_URI'] != '/login' && $_SERVER['REQUEST_URI'] != '/tologin') {
+                return redirect('/login');
+            }
+        };
 
         return $next($request);
     }
