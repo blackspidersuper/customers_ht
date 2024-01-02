21f282e02a01fcfe3197d5d9e3ff9fa7c96e774d
blackspider <blackspiderking666@gmail.com>
2024-01-02 16:49:51 +0800

    up

--- application/http/middleware/Check.php
+++ application/http/middleware/Check.php
@@ -10,15 +10,23 @@ class Check
     {
         $uid = Session::get('uid_sign');
 
-        $requestUri = ltrim($_SERVER['REQUEST_URI'], '/'); 
-        
-        $arr = array('douyin_table_all','baidu_table_check','baidu_table_info','kuaishou_table_all','gdt_click_table_all','gdt_exposure_table_all','gdt_follow_table_all','gdt_effect_table_all','gdt_fans_table_all');
-        
-        if (!in_array($requestUri, $arr)) {
-             if (empty($uid) && $_SERVER['REQUEST_URI'] != '/login' && $_SERVER['REQUEST_URI'] != '/tologin') {
+
+        $arr = array('/douyin_table_all', '/baidu_table_check', '/baidu_table_info', '/kuaishou_table_all', '/gdt_click_table_all', '/gdt_exposure_table_all', '/gdt_follow_table_all', '/gdt_effect_table_all', '/gdt_fans_table_all');
+
+        $str =$_SERVER['REQUEST_URI'];
+        $position = strpos($str, '?'); // 查找问号的位置  
+        if ($position !== false) {
+            $result = substr($str, 0, $position); // 截取字符串直到问号的位置  
+        } else {
+            $result = $str; // 如果没有找到问号，则保留原始字符串  
+        }
+
+        if (empty($uid) && $_SERVER['REQUEST_URI'] != '/login' && $_SERVER['REQUEST_URI'] != '/tologin') {
+            if (!in_array($result, $arr)) {
                 return redirect('/login');
             }
-        };
+        }
+
 
         return $next($request);
     }
